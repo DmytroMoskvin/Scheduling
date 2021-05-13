e69b1d2fd7a5e11321a954b1a6c61df281f8295a
Terbi-hub <ternovetskiyukr@gmail.com>
2021-05-13 09:43:20 +0300

    modify App

--- Scheduling/ClientApp/src/components/calendarComponent/app/bigcalendar/App.js
+++ Scheduling/ClientApp/src/components/calendarComponent/app/bigcalendar/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import { useState } from 'react';
 import events from "./events";
-import CalendarEvent from '../popup/CalendarEvent';
+import CalendarPopUp from '../popup/CalendarPopUp';
 //import Layout from 'react-tackle-box/Layout'
 import moment from "moment";
 import "./react-big-calendar.css";
-import { Button } from 'reactstrap';
+
+import Calendar2 from "../calendar/index";
+
+import 'moment/locale/en-gb'
 //import ExampleControlSlot from '../ExampleControlSlot'
 //let allViews = Object.keys(Views).map(k => Views[k])
 
@@ -17,11 +20,13 @@ import { Button } from 'reactstrap';
     },
   })*/
 moment.locale('en-gb', {
-  week: {
-    dow: 1,
-    doy: 1,
-  },
+    week: {
+        dow: 1,
+        doy: 1,
+    },
 });
+
+let reg = /\d+/g;
 
 const localizer = momentLocalizer(moment)
 class App extends React.Component {
@@ -29,61 +34,83 @@ class App extends React.Component {
         super(props);
         this.state = {
             events: events,
-            active: false
+            dayToday: new Date()
         };
 
-      
-    };    
-    
-  resizeEvent = (resizeType, { event, start, end }) => {
-    const { events } = this.state;
+        this.handleClick = this.handleClick.bind(this);
+        this.changeDay = this.changeDay.bind(this);
+    };
 
-    const nextEvents = events.map((existingEvent) => {
-      return existingEvent.id === event.id
-        ? { ...existingEvent, start, end }
-        : existingEvent;
-    });
-       
-    this.setState({
-      events: nextEvents
-    });
-  };
-    
-    
-  render() {
-      return (
-          <>
-              <CalendarEvent />
-      <Calendar
-        /*  events={this.state.events}
-            views={Views.WEEK}
-            step={60}
-            showMultiDayTimes
-            //max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
-            defaultDate={new Date(2015, 3, 1)}
-        */
-        min={new Date(0, 0, 0, 7, 0, 0)}
-        max={new Date(2021, 5, 20, 22, 0, 0)}
-        events={this.state.events}
-        nEventDrop={this.moveEvent}
-        //resizable
+    handleClick() {
+        this.setState(state => ({
+            active: !state.active
+        }));
+    }
 
-        onEventResize={this.resizeEvent}
-        onView={Views.WEEK}
-        views={Views.WEEK}
-        defaultView={Views.WEEK}
+    changeDay(day) {
+        this.setState({
+            dayToday: day
+        });
+    }
 
-        //defaultView={BigCalendar.Views.WEEK}
-        //  defaultDate={new Date(2015, 3, 13, 7, 0, 0)}
-        //defaultDate = {new Date(new Date().setHours(new Date().getHours()))}
-        //Date = {new Date(2021,5,20,0,0,0)}
-        localizer={localizer}
-                  culture={'en-GB'} />
-              
-         </>
-        
-    )
-  }
+    resizeEvent = (resizeType, { event, start, end }) => {
+        const { events } = this.state;
+
+        const nextEvents = events.map((existingEvent) => {
+            return existingEvent.id === event.id
+                ? { ...existingEvent, start, end }
+                : existingEvent;
+        });
+
+        this.setState({
+            events: nextEvents
+        });
+    };
+
+
+    render() {
+        return (
+            <>
+                <Calendar2 value={this.props.value} onChange={this.props.onChange} setDay={this.changeDay} />
+                <CalendarPopUp />
+
+                <Calendar
+                    /*  events={this.state.events}
+                        views={Views.WEEK}
+                        step={60}
+                        showMultiDayTimes
+                        //max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+                        defaultDate={new Date(2015, 3, 1)}
+                    */
+                    min={new Date(0, 0, 0, 7, 0, 0)}
+                    max={new Date(0, 0, 0, 22, 0, 0)}
+                    events={this.state.events}
+                    nEventDrop={this.moveEvent}
+                    //resizable
+
+                    onEventResize={this.resizeEvent}
+                    onView={Views.WEEK}
+                    views={Views.WEEK}
+                    defaultView={Views.WEEK}
+
+                    date={new Date(
+                        parseInt(this.state.dayToday.toString().match(reg)[2]),
+                        parseInt(this.state.dayToday.toString().match(reg)[0]) - 1,
+                        parseInt(this.state.dayToday.toString().match(reg)[1]))}
+                    onNavigate={date => { this.setState({ selectedDate: date }) }}
+                    //defaultView={BigCalendar.Views.WEEK}
+                    //defaultDate={this.state.dayToday}
+                    //defaultDate = {new Date(new Date().setHours(new Date().getHours()))}
+                    //Date = {new Date(2021,5,20,0,0,0)}
+                    localizer={localizer}
+                    culture={'en-GB'} />
+                <div>
+                    {this.state.dayToday.toString().match(reg)[0]}
+                </div>
+            </>
+
+        )
+    }
 }
 
 
